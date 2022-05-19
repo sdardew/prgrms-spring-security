0ba180c1bd3e689cb960a719dcd61fefc6fb2780
sdardew <sdardew@gmail.com>
2022-05-19 15:51:51 +0900

    w10d4

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -5,13 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -36,67 +40,71 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     auth.inMemoryAuthentication()
       .withUser("user").password("{noop}user123").roles("USER")
       .and()
-      .withUser("admin").password("{noop}admin123").roles("ADMIN");
+      .withUser("admin01").password("{noop}admin123").roles("ADMIN")
+      .and()
+      .withUser("admin02").password("{noop}admin123").roles("ADMIN")
+    ;
+  }
+
+  @Bean
+  public AccessDeniedHandler accessDeniedHandler() {
+    return (request, response, e) -> {
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      Object principal = authentication != null ? authentication.getPrincipal() : null;
+      log.warn("{} is denied", principal, e);
+      response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+      response.setContentType("text/plain;charset=UTF-8");
+      response.getWriter().write("ACCESS DENIED");
+      response.getWriter().flush();
+      response.getWriter().close();
+    };
+  }
+
+  public SecurityExpressionHandler<FilterInvocation> expressionHandler() {
+    return new CustomWebSecurityExpressionHandler(new AuthenticationTrustResolverImpl(), "ROLE_");
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
       .authorizeRequests()
-        .antMatchers("/me").hasAnyRole("USER", "ADMIN")
-        .antMatchers("/admin").access("isFullyAuthenticated() and hasRole('ADMIN')")
-        .anyRequest().permitAll()
-        .and()
+      .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+      .antMatchers("/admin").access("isFullyAuthenticated() and hasRole('ADMIN') and oddAdmin")
+      .anyRequest().permitAll()
+      .expressionHandler(expressionHandler())
+      .and()
       .formLogin()
-        .defaultSuccessUrl("/")
-        .permitAll()
-        .and()
+      .defaultSuccessUrl("/")
+      .permitAll()
+      .and()
       /**
        * remember me 설정
        */
       .rememberMe()
-        .rememberMeParameter("remember-me")
-        .tokenValiditySeconds(300)
-        .and()
+      .rememberMeParameter("remember-me")
+      .tokenValiditySeconds(300)
+      .and()
       /**
        * 로그아웃 설정
        */
       .logout()
-        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-        .logoutSuccessUrl("/")
-        .invalidateHttpSession(true)
-        .clearAuthentication(true)
-        .and()
+      .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+      .logoutSuccessUrl("/")
+      .invalidateHttpSession(true)
+      .clearAuthentication(true)
+      .and()
       /**
        * HTTP 요청을 HTTPS 요청으로 리다이렉트
        */
       .requiresChannel()
-        .anyRequest().requiresSecure()
-        .and()
+      .anyRequest().requiresSecure()
+      .and()
+      /**
+       * 예외처리 핸들러
+       */
       .exceptionHandling()
-        .accessDeniedHandler(accessDeniedHandler())
-
-      /*.requiresChannel()
-        .anyRequest().requiresSecure()
-        .and()
-      .anonymous()
-        .principal("thisIsAnonymousUser")
-        .authorities("ROLE_ANONYMOUS", "ROLE_UNKNOWN")*/
-
+      .accessDeniedHandler(accessDeniedHandler())
     ;
   }
 
-  @Bean
-  public AccessDeniedHandler accessDeniedHandler() {
-    return (httpServletRequest, httpServletResponse, e) -> {
-      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-      Object principal = authentication == null ? authentication.getPrincipal() : null;
-      log.warn("{} is denied", e);
-      httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
-      httpServletResponse.setContentType("text/plain");
-      httpServletResponse.getWriter().write("## ACCESS DENIED ##");
-      httpServletResponse.getWriter().flush();
-      httpServletResponse.getWriter().close();
-    };
-  }
 }
