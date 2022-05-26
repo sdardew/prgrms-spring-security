17474f4f1f65654194259d581c35442824babdc1
sdardew <sdardew@gmail.com>
2022-05-26 09:48:30 +0900

    s11d1

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -4,25 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.access.expression.SecurityExpressionHandler;
-import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.FilterInvocation;
-import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -30,38 +21,20 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
   private final Logger log = LoggerFactory.getLogger(getClass());
 
-  @Override
-  public void configure(WebSecurity web) {
-    web.ignoring().antMatchers("/assets/**");
+  private final DataSource dataSource;
+
+  public WebSecurityConfigure(DataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
   @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-      .withUser("user").password("{noop}user123").roles("USER")
-      .and()
-      .withUser("admin01").password("{noop}admin123").roles("ADMIN")
-      .and()
-      .withUser("admin02").password("{noop}admin123").roles("ADMIN")
-    ;
+  public void configure(WebSecurity web) {
+    web.ignoring().antMatchers("/assets/**", "/h2-console/**");
   }
 
   @Bean
-  public AccessDeniedHandler accessDeniedHandler() {
-    return (request, response, e) -> {
-      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-      Object principal = authentication != null ? authentication.getPrincipal() : null;
-      log.warn("{} is denied", principal, e);
-      response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-      response.setContentType("text/plain;charset=UTF-8");
-      response.getWriter().write("ACCESS DENIED");
-      response.getWriter().flush();
-      response.getWriter().close();
-    };
-  }
-
-  public SecurityExpressionHandler<FilterInvocation> expressionHandler() {
-    return new CustomWebSecurityExpressionHandler(new AuthenticationTrustResolverImpl(), "ROLE_");
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
   @Override
@@ -69,13 +42,17 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     http
       .authorizeRequests()
       .antMatchers("/me").hasAnyRole("USER", "ADMIN")
-      .antMatchers("/admin").access("isFullyAuthenticated() and hasRole('ADMIN') and oddAdmin")
+      .antMatchers("/admin").access("isFullyAuthenticated() and hasRole('ADMIN')")
       .anyRequest().permitAll()
-      .expressionHandler(expressionHandler())
       .and()
       .formLogin()
       .defaultSuccessUrl("/")
       .permitAll()
+      .and()
+      /**
+       * Basic Authentication 설정
+       */
+      .httpBasic()
       .and()
       /**
        * remember me 설정
@@ -106,5 +83,4 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
       .accessDeniedHandler(accessDeniedHandler())
     ;
   }
-
 }
